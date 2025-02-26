9da2009a4311d18c5bf99d8285e93f4917326932
Damianmdcf <damianmdcf@gmail.com>
2025-02-26 11:03:04 +0100

    Last improvements to code

--- util/img_util.py
+++ util/img_util.py
@@ -42,13 +42,11 @@ class ImageDataLoader:
         i= 1179
 
         # get a sorted list of all files in the directory
-        self.file_list= sorted([os.path.join(directory, f) for f in os.listdir(directory) if f.lower().endswith(('.png', '.jpg', 'jpeg', '.bmp', '.tiff'))])
+        # self.file_list= sorted([os.path.join(directory, f) for f in os.listdir(directory) if f.lower().endswith(('.png', '.jpg', 'jpeg', '.bmp', '.tiff'))])
 
-        self.file_list= sorted([os.path.join(directory, f) for f in os.listdir(directory) if f.lower().endswith(('.png', '.jpg', 'jpeg', '.bmp', '.tiff') and f in 'image_'+ i)])
-
-        for _ in range(1,201): 
-            i += 1
-            
+        # self.file_list= sorted([os.path.join(directory, f) for f in os.listdir(directory) for i in range(1179, 1379) if f.lower().endswith(('.png', '.jpg', 'jpeg', '.bmp', '.tiff') and i in f)])
+   
+        self.file_list= sorted([os.path.join(directory, f) for f in os.listdir(directory) if f.lower().endswith(('.png', '.jpg', 'jpeg', '.bmp', '.tiff')) and any(str(i) in f for i in range(1179, 1379))])
 
         if not self.file_list:
             raise ValueError("No image files found in the directory.")
@@ -79,7 +77,7 @@ class ImageDataLoader:
                 img_gray= self.transform(img_gray)
                 img_rgb= self.transform(img_rgb)
   
-Images= ImageDataLoader('C:/Users/bruda/2025-FYP-group10/moles')
+Images= ImageDataLoader('C:/Users/bruda/2025-FYP-group10/data')
 
 
 Images.__iter__()
